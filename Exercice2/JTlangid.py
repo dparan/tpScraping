875aa47c74405d0d6641744452b437fce468205d
Jonathan <jonathan.derouet2@gmail.com>
2019-11-20 16:23:01 +0100

    Fix exercice 2

--- Exercice2/JTlangid.py
+++ Exercice2/JTlangid.py
@@ -15,15 +15,15 @@ def convertISO(ISOcode):
     return switcher.get(ISOcode, "Invalid ISO code")
 
 
-fileList = os.listdir("Corpus_detourage/html")
+fileList = os.listdir("../Corpus_detourage/html")
 for f in fileList:
-    filename: str = 'Corpus_detourage/html/' + f
+    filename: str = '../Corpus_detourage/html/' + f
     response = open(filename, "r", encoding="UTF-8", errors="ignore").read()
     lang = langid.classify(response)
     paragraphs = justext.justext(response, justext.get_stoplist(convertISO(lang[0])))
     for paragraph in paragraphs:
         if not paragraph.is_boilerplate:
-            newFileName = "Corpus_detourage/JT_langid/" + f + ".txt"
+            newFileName = "../Corpus_detourage/JT_langid/" + f + ".txt"
             newFile = open(newFileName, "a", encoding="UTF-8")
             newFile.write("<p>" + paragraph.text + "</p>\n")
             newFile.close()
--- Exercice2/JTtrueLg.py
+++ Exercice2/JTtrueLg.py
@@ -2,12 +2,12 @@ import os
 import justext
 import json
 
-jsonFile = open("Corpus_detourage/doc_lg.json", "r")
+jsonFile = open("../doc_lg.json", "r")
 json = json.load(jsonFile)
 
-fileList = os.listdir("Corpus_detourage/html")
+fileList = os.listdir("../Corpus_detourage/html")
 for f in fileList:
-    filename: str = 'Corpus_detourage/html/' + f
+    filename: str = '../Corpus_detourage/html/' + f
     response = open(filename, "r", encoding="UTF-8", errors="ignore").read()
 
     language = "English"
@@ -17,7 +17,7 @@ for f in fileList:
     paragraphs = justext.justext(response, justext.get_stoplist(language))
     for paragraph in paragraphs:
         if not paragraph.is_boilerplate:
-            newFileName = "Corpus_detourage/JT_trueLg/" + f + ".txt"
+            newFileName = "../Corpus_detourage/JT_trueLg/" + f + ".txt"
             newFile = open(newFileName, "a", encoding="UTF-8")
             newFile.write("<p>" + paragraph.text + "</p>\n")
             newFile.close()
